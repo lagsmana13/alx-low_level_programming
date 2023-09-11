82d47a24dd166ffe86486520f7f4d7fbceca406d
lagsmana13 <lagsmana@yahoo.com>
2023-09-11 05:28:16 -0700

    badory

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-/*
+/**
  * The main function is the entry point of the program.
  * It prints the message "Programming is like building a multilingual puzzle"
  * and returns 0 to indicate successful execution.
