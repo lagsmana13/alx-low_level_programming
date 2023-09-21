7941740c9316976d57a8a1bec46ba2b430f2605e
lagsmana13 <lagsmana@yahoo.com>
2023-09-21 00:27:54 -0700

    makana

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,8 @@
 * _strcat - concatenates two strings
 * @dest: input value
 * @src: input value
-**
-Return: void
+*
+*Return: void
 */
 char *_strcat(char *dest, char *src)
 {
@@ -14,8 +14,8 @@ i = 0;
 while (dest[i] != '\0')
 {
 i++;
-}j
-= 0;
+}
+j= 0;
 while (src[j] != '\0')
 {
 dest[i] = src[j];
