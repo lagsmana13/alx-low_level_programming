c2ba4cb7d62fb71ce5111f4e7d7c602937a62034
lagsmana13 <lagsmana@yahoo.com>
2023-09-21 03:18:56 -0700

    7

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -45,11 +45,11 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i) -'0';
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j) -'0';
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
@@ -61,8 +61,9 @@ return (0);
 digits++;
 j--;
 i--;
-} if (
-digits == size_r)
+}
+if
+(digits == size_r)
 return (0);
 *(r + digits) = '\0';
 rev_string(r);
