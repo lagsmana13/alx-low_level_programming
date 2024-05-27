f0e99266683b7ee48aa963b108e507a2a7d1722c
lagsmana13 <lagsmana@yahoo.com>
2024-05-27 04:59:23 -0700

    20XX

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,30 +1,29 @@
 #include "search_algos.h"
 
 /**
-  * lookup_linear - Finds a number in a series
-  *                 of digits using lookup linear.
-  * @series: A reference to the starting element of the series to find.
-  * @count: The quantity of digits in the series.
-  * @number: The number to find.
-  *
-  * Return: If the number is absent or the series is NULL, -1.
-  *         Otherwise, the primary position where the number is found.
-  *
-  * Explanation: Displays a number each time it is evaluated in the series.
-  */
+ * lookup_linear - Finds number in series of digits using the Lookup linear method
+ *
+ * @series: Series to find
+ *
+ * @count: Count of the series
+ *
+ * @number: Number to find
+ *
+ * Return: Primary position where number is found or -1 for NULL series
+ */
+
 int lookup_linear(int *series, size_t count, int number)
 {
-	size_t j;
+	unsigned int position;
 
-	if (series == NULL)
-		return (-1);
-
-	for (j = 0; j < count; j++)
+	for (position = 0; series && position < count; position++)
 	{
-		printf("Number evaluated series[%ld] = [%d]\n", j, series[j]);
-		if (series[j] == number)
-			return (j);
-	}
+		printf("Number evaluated series[%d] = [%d]\n", position, series[position]);
 
+		if (series[position] == number)
+		{
+			return (position);
+		}
+	}
 	return (-1);
 }
